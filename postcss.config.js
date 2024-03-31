af4a4fe6891e7e2a8fc30c26a4a5a5d03cb4acfd
Biswabijaya Samal <me@biswabijaya.com>
2024-03-31 04:53:47 +0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,6 @@
-import process from "process"
-
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    autoprefixer: {}
   },
 }
