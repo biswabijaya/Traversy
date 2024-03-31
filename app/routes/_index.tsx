9d8272a69a98d84ce12092b948f7c9bbe538b6bb
Biswabijaya Samal <me@biswabijaya.com>
2024-03-31 14:42:51 +0300

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,13 @@
-export default function Portal() {
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Home | Welcome to My Remix App" },
+    { name: "description", content: "Welcome to my Remix!" },
+  ];
+};
+
+export default function Index() {
   return (
     <h1 className="text-3xl font-bold underline">
       Home
